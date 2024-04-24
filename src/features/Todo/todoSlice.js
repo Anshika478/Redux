c66cced2296320db9a5b280cd72592d54381ff6c
Anshika <AnshikaPatel47823@gmail.com>
2024-04-24 15:57:14 +0530

    Redux files

---
+++ src/features/Todo/todoSlice.js
@@ -0,0 +1,33 @@
+//reducers
+import { createSlice , nanoid} from '@reduxjs/toolkit'
+
+const initialState = {
+    todos : [{id : "abc" , task : "Demo Task" , isDone : false}] ,
+}
+
+export const todoSlice = createSlice({
+    name : "todo" ,
+    initialState ,
+    reducers : { //state , action
+        addTodo : (state , action) => {
+            const newTodo = {
+                id : nanoid() , task : action.payload , isDone : false 
+            }
+            state.todos.push(newTodo) ;//direct mutation
+        } , 
+        deleteTodo : (state , action) => {
+            state.todos = state.todos.filter((todo) => todo.id != action.payload) ;
+        } ,
+        markAsDone : (state , action) => {
+            state.todos = state.todos.map((todo) => {
+                if(todo.id === action.payload) {
+                    todo.isDone = true ;
+                }
+            })
+        }
+    }
+}) ;
+
+//to create action objects
+export const {addTodo , deleteTodo , markAsDone} = todoSlice.actions ;
+export default todoSlice.reducer ;
